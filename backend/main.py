cb0393261c9418475e48fd4eb6c9fd6586335252
gking <gkingt@126.com>
2024-07-27 15:43:23 +0800

    修改图标

--- backend/main.py
+++ backend/main.py
@@ -2260,7 +2260,7 @@ async def get_manifest_json():
         "display": "standalone",
         "background_color": "#343541",
         "orientation": "portrait-primary",
-        "icons": [{"src": "/static/logo.png", "type": "image/png", "sizes": "500x500"}],
+        "icons": [{"src": "/static/favicon_logo.png", "type": "image/png", "sizes": "500x500"}],
     }
 
 
